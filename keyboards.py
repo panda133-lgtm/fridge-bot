045a37158772e84a3d9cd5719ee0ec5efb8b6fac
panda133-lgtm <a.prekrasnaya133@gmail.com>
2026-04-01 10:43:52 +0300

    keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,28 +1,8 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup
-from urllib.parse import quote
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_main_menu():
+    """Главное меню внизу экрана"""
     kb = [
-        [KeyboardButton(text="📦 Список продуктов")],
-        [KeyboardButton(text="➕ Добавить вручную")]
+        [KeyboardButton(text="📦 Список продуктов"), KeyboardButton(text="➕ Добавить продукт")]
     ]
     return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
-
-def get_product_list_keyboard(products):
-    keyboard = []
-    
-    for name, qty in products:
-        status = "⚠️" if qty <= 3 else "✅"
-        encoded_name = quote(name)
-        
-        row = [
-            InlineKeyboardButton(text="➖", callback_data=f"dec_{encoded_name}"),
-            InlineKeyboardButton(text=f"{status} {name} ({qty})", callback_data="info"),
-            InlineKeyboardButton(text="➕", callback_data=f"inc_{encoded_name}"),
-            InlineKeyboardButton(text="🗑", callback_data=f"del_{encoded_name}")
-        ]
-        keyboard.append(row)
-    
-    keyboard.append([InlineKeyboardButton(text="🔄 Обновить список", callback_data="refresh_list")])
-    
-    return InlineKeyboardMarkup(inline_keyboard=keyboard)
